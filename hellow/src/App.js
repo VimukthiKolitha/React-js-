0a059175107e3c587521cadb45eb29aa36f93cab
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-31 12:52:01 +0530

    app.js (main file)

---
+++ hellow/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    //you have to send only one element via return 
+  <>
+  <div>
+    <h>Hellow</h>
+  </div>
+  </>
+  );
+}
+
+export default App;

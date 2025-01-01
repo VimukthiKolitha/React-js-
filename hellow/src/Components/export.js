fff66fb3297af33bf3cd325d33962e2befa2e8fb
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-01 11:24:26 +0530

    export file using export not (default Export)

---
+++ hellow/src/Components/export.js
@@ -0,0 +1,25 @@
+export function Data1 ()
+{
+    const obj1 = {
+        name:'Export Data 1',
+    }
+    return (
+        <>
+        <h3>Export file</h3>
+         <p>{obj1.name}</p>
+        </>
+    )
+}
+
+export function Data2 ()
+{
+    const obj1 = {
+        name:'Export Data 2',
+    }
+    return (
+        <>
+        
+         <p>{obj1.name}</p>
+        </>
+    )
+}

8a62ff58a7f9fbf55dab34ebbc73474d2f948d39
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-02 18:45:16 +0530

    image

---
+++ hellow/src/Components/cssUsing.js
@@ -0,0 +1,13 @@
+
+function F1 ()
+{
+    return(
+      <>
+      <img src="https://media.alpinecars.com/wp-content/uploads/2024/01/ec183209f124129301b0dba364ac3805.jpeg" alt="alpine"/>
+      <p>kegalle</p>
+      <p>polonnaruwa</p>
+      <p>jaffna</p>
+      </>
+    )
+}
+export default F1;

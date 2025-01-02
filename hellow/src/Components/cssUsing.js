224df29581131a49b7bf89b61863381af8f74815
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-02 21:47:53 +0530

    css use

--- hellow/src/Components/cssUsing.js
+++ hellow/src/Components/cssUsing.js
@@ -1,13 +1,12 @@
-
-function F1 ()
+import './F1.css';
+export function F1 ()
 {
     return(
       <>
-      <img src="https://media.alpinecars.com/wp-content/uploads/2024/01/ec183209f124129301b0dba364ac3805.jpeg" alt="alpine"/>
+      <img src="https://media.alpinecars.com/wp-content/uploads/2024/01/ec183209f124129301b0dba364ac3805.jpeg" alt="alpine" className='img'/>
       <p>kegalle</p>
       <p>polonnaruwa</p>
       <p>jaffna</p>
       </>
     )
 }
-export default F1;

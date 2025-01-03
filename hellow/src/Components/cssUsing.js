6ee39dd044fd41ab0bff42c64bb6032f2a1c3290
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-03 19:43:30 +0530

    updated

--- hellow/src/Components/cssUsing.js
+++ hellow/src/Components/cssUsing.js
@@ -7,6 +7,13 @@ export function F1 ()
       <p>kegalle</p>
       <p>polonnaruwa</p>
       <p>jaffna</p>
+      {/*inline css*/}
+      <button style={{
+        fontSize:'10px',
+        border: '1px solid black',
+        padding: '7px 10px',
+      
+      }}>Submit</button>
       </>
     )
 }

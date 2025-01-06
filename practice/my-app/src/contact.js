7980f2599066404ee6853eda3c9e492ea71fb7b6
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-06 21:32:55 +0530

    contact page

---
+++ practice/my-app/src/contact.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import NavBar from './navBar'
+
+export default function contact() {
+  return (
+    <div>
+    <NavBar/>
+        <h1>Contact US</h1>
+
+        <p>Contact Us
+We’re here to help and answer any questions you might have. Whether you're looking for more information, need assistance, or want to provide feedback, we’d love to hear from you.
+
+Get in Touch
+Feel free to reach out to us using any of the methods below:
+
+Email: [your-email@example.com]
+Phone: [Your Phone Number]
+Address: [Your Office Address]
+Business Hours
+Monday to Friday: 9:00 AM – 6:00 PM
+Saturday: 10:00 AM – 4:00 PM
+Sunday: Closed
+Social Media
+Connect with us on social media:
+
+Facebook: [Your Facebook Page Link]
+Instagram: [Your Instagram Profile Link]
+Twitter: [Your Twitter Handle]</p>
+    </div>
+  )
+}

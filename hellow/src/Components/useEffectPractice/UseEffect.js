eb34079d77bb23cc6f554b0d6f40350b66aeabf9
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-08 21:58:01 +0530

    use effect use

--- hellow/src/Components/useEffectPractice/UseEffect.js
+++ hellow/src/Components/useEffectPractice/UseEffect.js
@@ -5,10 +5,15 @@ function UseEffect() {
     const [data,setdata] = useState({});
     
     console.log(apiID);
+
+    //console data
     console.log(data);
 
+    //use effect
     useEffect(() =>{
         console.log('Use effect running');
+
+        //if api length > 0 run this part
      if(apiID.length > 0)
      {
         
@@ -16,22 +21,45 @@ function UseEffect() {
         
         //fetch data 
         const apiCall = async() =>{
+
+          //to mesure how many tome apiCall runs
+          console.log('api call running');
             const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${apiID}`);
               const data = await res.json()
+              
+              //if there any data set to that data to data
+              if(data)
+              {
+                setdata(data);
+              }
         }
-        if(data)
+        //validate (apiId should be betweeen 0 and 100)
+        if(apiID.length > 0 && Number(apiID) > 0 && Number(apiID) < 100)
         {
-            setdata(data);
-        }
+          //call api function 
          apiCall();
+        }
+        //cleanup function
+          return() =>
+          {
+            console.log('cleanup');
+             apiCall();
+          }
      }
-
+     //this use effect runs only when there any data in api id
     },[apiID])
   return (
     <div>
         <input type='text' placeholder='Enter id:' value={apiID} onChange={((e)=>setapiID(e.target.value))}  style={{
             border:'1px solid black'
         }}/>
+
+        {data && (
+          <div>
+            <h1>{data.title}</h1>
+            <h1>{data.id}</h1>
+          </div>
+        )}
     </div>
   )
 }

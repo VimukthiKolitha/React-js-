cf27f9cdeb0344bce82383e56151a7678e69852a
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-22 21:55:20 +0530

    use effect Hook usage

---
+++ hellow/src/Components/UseEffect/EffectHook.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+function EffectHook() {
+
+    const  [click,setclick] = useState(0);
+    let  [count,setcount] = useState(0);
+
+    const clicCount = () =>{
+          setclick(click + 1);
+    }
+    const test = () =>{
+        setcount(count = click)
+    }
+    //every time something change use effect will be work
+    useEffect(()=>{
+        document.title = `you click ${count} times` //when click test btn this will set 'title as you click ${count} times'
+    },[count]);
+  return (
+    <div>
+        <button style={{border:'1px solid black'}} onClick={clicCount}>Click</button>
+        <button style={{border:'1px solid black'}} onClick={test}>Test</button>
+        <div>{click}</div>
+    </div>
+  )
+}
+
+export default EffectHook
